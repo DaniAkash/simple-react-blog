b9487cb22e6069213a403641b1aa6a86af9b206f
DaniAkash <s.daniakash@gmail.com>
2019-09-08 14:58:43 +0530

    create navbar toggle

--- src/App.js
+++ src/App.js
@@ -9,13 +9,27 @@ import {
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
+
+  state = {
+    isNavbarOpen: false,
+  };
+
+  toggleNavBar = () => {
+    this.setState({
+      isNavbarOpen: !this.state.isNavbarOpen
+    });
+  };
+
   render() {
+
+    const {isNavbarOpen} = this.state;
+
     return (
       <div>
         <Navbar color="faded" light toggleable>
-        <NavbarToggler right onClick={() => null} />
+        <NavbarToggler right onClick={this.toggleNavBar} />
           <a className="navbar-brand" href={"home"}>Blog</a>
-          <Collapse isOpen={true} navbar>
+          <Collapse isOpen={isNavbarOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
                 <a className="nav-link" activeClassName="active" href={"home"}>Home</a>
