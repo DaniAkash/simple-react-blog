32b129cdef2f27eece13f5aa05786bd60ab8274f
DaniAkash <s.daniakash@gmail.com>
2019-09-08 14:32:30 +0530

    create a navigation header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+  Collapse, 
+  Navbar, 
+  NavbarToggler, 
+  Nav, 
+  NavItem
+} from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light toggleable>
+        <NavbarToggler right onClick={() => null} />
+          <a className="navbar-brand" href={"home"}>Blog</a>
+          <Collapse isOpen={true} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <a className="nav-link" activeClassName="active" href={"home"}>Home</a>
+              </NavItem>
+              <NavItem>
+                <a className="nav-link" activeClassName="active" href={"home"}>Authors</a>
+              </NavItem>
+              <NavItem>
+                <a className="nav-link" activeClassName="active" href={"home"}>New Post</a>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+  }
 }
 
 export default App;
