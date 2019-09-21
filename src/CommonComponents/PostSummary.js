b786645238d6606207409aef6e4f86aae0b68595
DaniAkash <s.daniakash@gmail.com>
2019-09-21 12:22:46 +0530

    render posts summary

---
+++ src/CommonComponents/PostSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostSummary = ({
+    id,
+    author,
+    content,
+    title,
+    datetime,
+}) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <p>{author}</p>
+            <p>{content}</p>
+        </div>
+    )
+};
+
+PostSummary.propTypes = {
+    id: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    datetime: PropTypes.string.isRequired,
+};
+
+export default PostSummary;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
 import fetchData from '../../services/fetchData';
 import { GET_POSTS_API } from '../../constants/serverUrls';
+import PostSummary from '../../CommonComponents/PostSummary';
 
 class Home extends Component {
+
+    state = {
+        posts: []
+    };
 
     componentDidMount() {
         this.loadPostData();
@@ -11,15 +16,29 @@ class Home extends Component {
     loadPostData = async () => {
         try {
             const posts = await fetchData(GET_POSTS_API, "GET");
-            console.log(posts);
+            this.setState({ posts });
         } catch (e) {
             console.error(e);
         }
     };
 
     render() {
+
+        const { posts } = this.state;
+
         return (
-            <div>Home!</div>
+            <div>
+                {posts.map((post, postIndex) => {
+                    return (
+                        <PostSummary
+                            id={post.id}
+                            title={post.title}
+                            content={post.content}
+                            author={post.author}
+                        />
+                    )
+                })}
+            </div>
         );
     }
 }
