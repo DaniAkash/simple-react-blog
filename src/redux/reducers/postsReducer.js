21517ca2c04799a3b9fbcfc100b6ba055a888f7d
DaniAkash <s.daniakash@gmail.com>
2019-10-13 10:33:31 +0530

    create posts reducer

---
+++ src/redux/reducers/postsReducer.js
@@ -0,0 +1,24 @@
+import initialState from "../store/initialState";
+import { ACTION_GET_POSTS } from "../actionTypes/actionTypes";
+
+/**
+ * state - previous state 
+ * action - value returned by your actions
+ */
+const postsReducer = (state = initialState.posts, action) => {
+
+    switch(action.type) {
+
+        case ACTION_GET_POSTS:
+            return action.payload;
+
+        /**
+         * default should always return the previous state
+         */
+        default:
+            return state;
+    }
+
+};
+
+export default postsReducer;
