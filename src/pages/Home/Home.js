6fbb2683ffac81639c146b0988e89cecc228b96d
DaniAkash <s.daniakash@gmail.com>
2019-10-13 11:39:37 +0530

    load post data from redux

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,27 +6,31 @@ import LoadingIndicator from '../../CommonComponents/LoadingIndicator';
 import ErrorScreen from '../ErrorScreen/ErrorScreen';
 import ErrorBoundaryV2 from '../../HigherOrderComponents/ErrorBoundaryV2';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getAllPosts } from '../../redux/actions/postActions';
+
 class Home extends Component {
 
-    state = {
-        posts: [],
-    };
+    // state = {
+    //     posts: [],
+    // };
 
     componentDidMount() {
-        this.loadPostData();
+        this.props.loadPostData();
     }
 
-    loadPostData = async () => {
-        try {
-            const posts = await fetchData(GET_POSTS_API, "GET");
-            this.setState({ posts });
-        } catch (e) {
-            console.error(e);
-        }
-    };
+    // loadPostData = async () => {
+    //     try {
+    //         const posts = await fetchData(GET_POSTS_API, "GET");
+    //         this.setState({ posts });
+    //     } catch (e) {
+    //         console.error(e);
+    //     }
+    // };
 
     render() {
-        const { posts } = this.state;
+        const { posts } = this.props;
 
         return (
             <div>
@@ -52,4 +56,37 @@ class Home extends Component {
     }
 }
 
-export default ErrorBoundaryV2(Home);
+const mapStateToProps = state => { // Sending data from store into react component
+    return {
+        posts: state.posts,
+    };
+};
+
+const mapDispatchToProps = dispatch => { // Sending the actions we created into the react component
+    return {
+        loadPostData: bindActionCreators(getAllPosts, dispatch)
+    };
+};
+
+const connectHOC = connect(
+    mapStateToProps, 
+    mapDispatchToProps // optional
+);
+
+export default connectHOC(ErrorBoundaryV2(Home));
+
+/**
+
+// Normal HOC
+function (ReactComponent) {
+    return ImprovedReactComponent;
+}
+
+// connect HOC
+function( function1, function2 ) {
+    return function (ReactComponent) {
+        return ImprovedReactComponent;
+    }
+}
+
+*/
